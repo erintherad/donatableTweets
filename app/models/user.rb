2706fd115ec5983bdeeba13aa57bfef66f1aa7a5
Erin Mahoney <erin@heapanalytics.coml>
2017-02-05 10:28:27 -0800

    Added user tests and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :twitter_handle, presence: true, uniqueness: true
+  validates :full_name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User has Twitter Handle" do
+    user = User.create(twitter_handle: nil, full_name: "Erin Mahoney")
+
+    assert_equal false, user.valid?
+
+    assert_equal "Twitter handle can't be blank", user.errors.full_messages.first
+  end
+
+  test "Twitter handle is unique" do
+    user1 = User.create!(twitter_handle: "erintherad", full_name: "Erin Mahoney")
+    user2 = User.create(twitter_handle: "erintherad", full_name: "Erin Mahoney")
+
+    assert_equal false, user2.valid?
+
+    assert_equal "Twitter handle has already been taken", user2.errors.full_messages.first
+  end
+
+  test "User has name" do
+    user = User.create(twitter_handle: "erintherad", full_name: nil)
+
+    assert_equal false, user.valid?
+
+    assert_equal "Full name can't be blank", user.errors.full_messages.first
+  end
 end
